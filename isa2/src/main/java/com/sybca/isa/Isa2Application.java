c4dd892771e7dc2f7182bcfd1024bd70173e729e
Zapdc <101114200+Zapdc@users.noreply.github.com>
2025-03-28 16:46:20 +0530

    Add files via upload

---
+++ isa2/src/main/java/com/sybca/isa/Isa2Application.java
@@ -0,0 +1,13 @@
+package com.sybca.isa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isa2Application.class, args);
+	}
+
+}

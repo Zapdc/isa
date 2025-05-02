f6d97863d085a97e9533f28cc0bfe0c54479c26e
Zapdc <101114200+Zapdc@users.noreply.github.com>
2025-05-02 10:07:57 +0530

    Add files via upload

---
+++ isa2/src/test/java/com/sybca/isa/Isa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sybca.isa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
